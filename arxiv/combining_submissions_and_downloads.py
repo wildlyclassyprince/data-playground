ac305b47f68a996f1801b0dbeefee21271f0f808
wildlyclassyprince <lihtumb@gmail.com>
2018-08-28 14:53:41 +0200

    Edited .py file

--- arxiv/combining_submissions_and_downloads.py
+++ arxiv/combining_submissions_and_downloads.py
@@ -5,26 +5,26 @@ arXiv Monthly Submissions & Downloads
 -------------------------------------
 
 This script takes the submissions and downloads files, and
-merges (right-join) them into a single file by pivoting on 
-the month (i.e., the date corresponding to the number of 
+merges (right-join) them into a single file by pivoting on
+the month (i.e., the date corresponding to the number of
 submissions or downloads on that month).
 
 '''
 
 # The usual suspects ...
-import pandas as pd
 import glob as gb
+import pandas as pd
 
 # Getting the data
-downloads = pd.concat([pd.read_csv(file)
-                       for file in 
+DOWNLOADS = pd.concat([pd.read_csv(file)
+                       for file in
                        gb.glob('*_downloads_*.csv')
                       ])
-submissions = pd.concat([pd.read_csv(file)
+SUBMISSIONS = pd.concat([pd.read_csv(file)
                          for file in
                          gb.glob('*_submissions_*.csv')
                         ])
-data = pd.merge(submissions, downloads, on='month')
+DATA = pd.merge(SUBMISSIONS, DOWNLOADS, on='month')
 
 # Create the new dataset
-data.to_csv('combined_submissions_and_downloads.csv', sep=',')
+DATA.to_csv('combined_submissions_and_downloads.csv', sep=',')
