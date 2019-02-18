dc758290bed7a2a6b5453b1f6399648e3cedb360
wildlyclassyprince <lihtumb@gmail.com>
2019-02-18 13:21:54 +0200

    Added retrieve-tweets.py

---
+++ zimbabwe_on_twitter/retrieve-tweets.py
@@ -0,0 +1,18 @@
+'''
+retrieve-tweets.py
+
+The purpose of this script is to retrieve
+tweets from Twitter using the Twitter API.
+
+We will need to authenticate with the
+Twitter API before we can do anything else.
+'''
+
+# Imports
+
+# Authentication
+
+# Run
+
+if __name__ == "__main__":
+    pass
