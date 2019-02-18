e36fe627d25e35551302662e867ff74c6f4e7f10
wildlyclassyprince <lihtumb@gmail.com>
2019-02-18 18:38:39 +0200

    Added file

---
+++ zimbabwe_on_twitter/transform-data.py
@@ -0,0 +1,24 @@
+'''
+transform-data.py
+
+The purpose of this script is to transform the collected
+data into a flexible form.
+
+We will:
+- read the data from the database
+- change datatypes
+
+'''
+
+# Imports
+import pandas as pd
+
+from * import retrieve-tweets
+
+# Read the data
+
+# Change datatypes
+
+# Run
+if if __name__ == "__main__":
+    pass
